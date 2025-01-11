a51c3ad68770970b9a9ee9f5e32b0f268c94f15b
Pipluppp <duncanb013@gmail.com>
2025-01-12 01:30:33 +0800

    Implement simple parser of function and variable declarations, statements are only semicolons

--- parser.c
+++ parser.c
@@ -11,7 +11,16 @@ int num_tokens;
 // Function prototypes
 Token *load_tokens(const char *filename, int *num_tokens);
 void parse_program();
-void parse_function();
+void parse_declaration();
+void parse_function_declaration();
+void parse_variable_declaration();
+void parse_array_declaration();
+void parse_data_type();
+void parse_identifier();
+void parse_parameter_list();
+void parse_block();
+void parse_block_item_list();
+void parse_block_item();
 void parse_statement();
 void parse_exp();
 void match(TokenType type);
@@ -34,33 +43,143 @@ void match(TokenType type) {
     if (current_token < num_tokens && tokens[current_token].type == type) {
         current_token++;
     } else {
-        printf("Error: Expected token type %s but found %s at line %d\n", 
+        printf("Error: Expected token type %s but found %s at line %d\n",
                token_names[type], token_names[tokens[current_token].type],
                tokens[current_token].line_number);
         exit(1);
     }
 }
 
-// <program> ::= <function>
+// <program> ::= { <declaration> }
 void parse_program() {
-    parse_function();
+    while (current_token < num_tokens && tokens[current_token].type != TOKEN_EOF) {
+        parse_declaration();
+    }
 }
 
-// <function> ::= "int" <identifier> "(" "void" ")" "{" <statement> "}"
-void parse_function() {
-    match(INT);
-    match(IDENTIFIER);
+// <declaration> ::= <variable_declaration> | <array_declaration> | <function_declaration>
+void parse_declaration() {
+    if (current_token < num_tokens && (tokens[current_token].type == INT || tokens[current_token].type == FLOAT ||
+                                       tokens[current_token].type == CHAR || tokens[current_token].type == BOOL)) {
+        parse_data_type();
+        parse_identifier();
+
+        if (current_token < num_tokens && tokens[current_token].type == LEFT_PARENTHESIS)
+        {
+            parse_function_declaration();
+        }
+        
+        else if(current_token < num_tokens && tokens[current_token].type == LEFT_BRACKET)
+        {
+            parse_array_declaration();
+        }
+        else{
+            parse_variable_declaration();
+        }
+        
+    } else {
+        printf("Error: Invalid declaration at line %d\n", tokens[current_token].line_number);
+        exit(1);
+    }
+}
+
+// <variable_declaration> ::= <data_type> <identifier> [ “=” <exp> ] “;”
+//                         | <data_type> <identifier> { “,” <identifier> } “;”
+void parse_variable_declaration(){
+    if (current_token < num_tokens && tokens[current_token].type == SEMICOLON) {
+        match(SEMICOLON);
+    } else if (current_token < num_tokens && tokens[current_token].type == ASSIGN) {
+        match(ASSIGN);
+        parse_exp();
+        match(SEMICOLON);
+    } else if (current_token < num_tokens && tokens[current_token].type == COMMA) {
+        while (current_token < num_tokens && tokens[current_token].type == COMMA) {
+            match(COMMA);
+            parse_identifier();
+        }
+        match(SEMICOLON);
+    } else {
+        printf("Error: Invalid variable declaration at line %d\n", tokens[current_token].line_number);
+        exit(1);
+    }
+}
+
+// <array_declaration> ::= <data_type> <identifier> “[“ [<const>]  “]”  [ “=” “{“ <argument_list>“}” ] “;”
+void parse_array_declaration(){
+    match(LEFT_BRACKET);
+    if (current_token < num_tokens && tokens[current_token].type == NUMBER) {
+        match(NUMBER);
+    }
+    match(RIGHT_BRACKET);
+    if (current_token < num_tokens && tokens[current_token].type == ASSIGN) {
+        match(ASSIGN);
+        match(LEFT_BRACE);
+        //parse_argument_list(); // You haven't implemented parse_argument_list yet
+        match(RIGHT_BRACE);
+    }
+    match(SEMICOLON);
+}
+
+// <function_declaration> ::= <data_type> <identifier> "(" <parameter_list> ")" ( <block> | “;” )
+void parse_function_declaration() {
     match(LEFT_PARENTHESIS);
+    parse_parameter_list();
     match(RIGHT_PARENTHESIS);
+    if (current_token < num_tokens && tokens[current_token].type == LEFT_BRACE) {
+        parse_block();
+    } else {
+        match(SEMICOLON);
+    }
+}
+
+// <parameter_list> ::= [“void”]
+//   | <data_type> <identifier> {"," <data_type> <identifier>}
+void parse_parameter_list() {
+    // For now, we'll only handle the [“void”] case
+    // You can expand this later to handle the full grammar
+    // if (current_token < num_tokens && tokens[current_token].type == VOID) {
+    //     match(VOID);
+    // }
+}
+
+// <data_type> ::= “int” | “float” | “char” | “bool”
+void parse_data_type() {
+    if (current_token < num_tokens && (tokens[current_token].type == INT || tokens[current_token].type == FLOAT ||
+                                       tokens[current_token].type == CHAR || tokens[current_token].type == BOOL)) {
+        match(tokens[current_token].type);
+    } else {
+        printf("Error: Expected data type at line %d\n", tokens[current_token].line_number);
+        exit(1);
+    }
+}
+
+// <identifier> ::= identifier token
+void parse_identifier() {
+    match(IDENTIFIER);
+}
+
+// <block> ::= "{" <block-item-list>  "}"
+void parse_block() {
     match(LEFT_BRACE);
-    parse_statement();
+    parse_block_item_list();
     match(RIGHT_BRACE);
 }
 
-// <statement> ::= "return" <exp> ";"
+// <block_item_list> ::= (<block_item_list> <block_item>) | <block_item>
+void parse_block_item_list() {
+    while (current_token < num_tokens && tokens[current_token].type != RIGHT_BRACE) {
+        parse_block_item();
+    }
+}
+
+// <block_item> ::= <statement> | <declaration>
+void parse_block_item() {
+    // Simplified for now, assuming only statements
+    parse_statement();
+}
+
+// <statement> ::= ";"
 void parse_statement() {
-    match(RETURN);
-    parse_exp();
     match(SEMICOLON);
 }
 
@@ -81,7 +200,7 @@ Token *load_tokens(const char *filename, int *num_tokens) {
         fprintf(stderr, "Error: Memory allocation failed!\n");
         exit(1);
     }
-    
+
     *num_tokens = 0;
 
     char line[256];
